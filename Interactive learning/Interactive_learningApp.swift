1d5dac721b5d19dbf6fd4f4489b391548a983cf7
Afeez <harfyzz@gmail.com>
2024-10-31 12:47:23 +0000

    Initial Commit

---
+++ Interactive learning/Interactive_learningApp.swift
@@ -0,0 +1,17 @@
+//
+//  Interactive_learningApp.swift
+//  Interactive learning
+//
+//  Created by Afeez Yunus on 31/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Interactive_learningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
