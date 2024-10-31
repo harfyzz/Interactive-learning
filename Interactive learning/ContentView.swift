4ddce6d8fa663543907ac3539b125e31bdc7fabc
Afeez <harfyzz@gmail.com>
2024-10-31 13:57:27 +0000

    -file set up

--- Interactive learning/ContentView.swift
+++ Interactive learning/ContentView.swift
@@ -6,16 +6,62 @@
 //
 
 import SwiftUI
+import RiveRuntime
 
 struct ContentView: View {
+    
+    @State var climber = RiveViewModel(fileName: "climber", stateMachineName: "main")
+    @State var option = RiveViewModel(fileName: "option_button", stateMachineName: "main")
+    var answer: Int = 0
+    var value1:Int = 0
+    var value2:Int = 0
+    @State var choice = 0
+    @State var isPressed: Bool = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        ZStack {
+            VStack{
+                climber.view()
+                    .frame(height: 511)
+                Spacer()
+            }
+            VStack {
+                Spacer()
+                VStack{
+                    HStack {
+                        let value1 = Int.random(in: 1...20)
+                        let value2 = Int.random(in: 1...20)
+                        Text("\(value1) + \(value2)")
+                            .font(.system(size: 55))
+                            .fontWeight(.medium)
+                        if isPressed {
+                            let answer = value1 + value2
+                            Text("\(answer)")
+                                .font(.largeTitle)
+                        }
+                    }.foregroundStyle(Color("text.main"))
+                    Spacer()
+                    VStack{
+                        HStack{
+                            option.view()
+                                .onAppear{
+                                    do {
+                                        try option.setTextRunValue("option text", textValue: "2")
+                                    } catch {
+                                        print("Error: \(error)")
+                                    }
+                                }
+                            option.view()
+                        }
+                    }
+                    Spacer()
+                }
+                .background(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 16))
+            }
+            
+        }.padding()
+            .background(Color("Background"))
+            .ignoresSafeArea()
     }
 }
 
