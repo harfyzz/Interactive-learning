93e20fdbcbe2a6c8888b11a79bc13bc059bda9fe
Afeez <harfyzz@gmail.com>
2024-10-31 16:16:26 +0000

    set up basic button change

--- Interactive learning/ContentView.swift
+++ Interactive learning/ContentView.swift
@@ -12,11 +12,13 @@ struct ContentView: View {
     
     @State var climber = RiveViewModel(fileName: "climber", stateMachineName: "main")
     @State var option = RiveViewModel(fileName: "option_button", stateMachineName: "main", fit:.contain)
+    @State var mainButton = RiveViewModel(fileName: "main_button", stateMachineName: "main", fit:.contain)
     @State var value1:Int = Int.random(in: 1...20)
     @State var value2:Int = Int.random(in: 1...20)
     @State var choice:[Int] = []
     @State var isPressed: Bool = false
     @State var answer: Int = 0
+    @State var selectedChoice: Int? = nil
     var correctAnswer: Int {
             value1 + value2
         }
@@ -40,18 +42,35 @@ struct ContentView: View {
                         .font(.system(size: 55))
                         .fontWeight(.medium)
                         .contentTransition(.numericText())
+                    Spacer()
                     LazyVGrid(columns: [GridItem(),GridItem()], spacing: 12) {
                         ForEach(choice, id: \.self) {choice in
-                            optionView(choice: choice) {
-                                withAnimation {
-                                    answer = choice
-                                    isPressed = true
+                            optionView(choice: choice, selectedChoice: $selectedChoice) {
+                                    withAnimation {
+                                        answer = choice
+                                        isPressed = true
+                                        selectedChoice = choice
+                                    }
                                 }
-                            }
                         }
                     }
-                    
+                    Spacer()
+                    mainButton.view()
+                        .frame(height:70)
+                        .onTapGesture {
+                            if answer == correctAnswer {
+                                mainButton.setInput("type", value: Double(2))
+                            } else {
+                                mainButton.setInput("type", value: Double(3))
+                            }
+                        }
+                        .onChange(of: isPressed) { oldValue, newValue in
+                            if isPressed {
+                                mainButton.setInput("type", value: Double(1))
+                            }
+                        }
                 }
+                .padding(.vertical, 32)
                 .frame(height: UIScreen.main.bounds.height/2)
                 .background(.white)
                     .clipShape(RoundedRectangle(cornerRadius: 16))
@@ -85,18 +104,25 @@ struct ContentView: View {
 }
 
 struct optionView: View {
-    @State var option = RiveViewModel(fileName: "option_button", stateMachineName: "main", fit:.contain)
-    @State var choice:Int
-    @State var action: ()->Void
+    @State var option = RiveViewModel(fileName: "option_button", stateMachineName: "main", fit: .contain)
+    @State var choice: Int
+    @Binding var selectedChoice: Int?
+    var action: () -> Void
+    
     var body: some View {
         option.view()
-            .frame(height:50)
-            .onAppear{
+            .frame(height: 50)
+            .onAppear {
                 try! option.setTextRunValue("option text", textValue: String(choice))
             }
-            .onTapGesture{
-                    option.triggerInput("pressed?")
+            .onTapGesture {
+                option.setInput("state", value: Double(1))
                 action()
+            }
+            .onChange(of: selectedChoice) { _, newValue in
+                if newValue != choice {
+                    option.setInput("state", value: Double(0)) // Reset unselected options
+                }
             }
     }
 }
